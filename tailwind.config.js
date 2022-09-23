b452a91ed286223ebd9611abd15ab35931be9296
Daouda Lo <daoudalothiemping@gmail.com>
2022-09-23 01:54:57 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#06b6d4',
+        secondary: {
+          100: '#9333ea',
+          200: '#2563eb'
+        },
+      },
+      fontSize: {
+        pro: '10rem'
+      },
+      fontFamily:{
+        nunitro: 'Nunito'
+      }
+    },
+  },
+  plugins: [],
+}
